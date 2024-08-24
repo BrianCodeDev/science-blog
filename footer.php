97509e6e77dea028ef9041b831e81f9afc2158da
Brian Kelley <107327227+BrianCodeDev@users.noreply.github.com>
2024-08-24 12:57:16 -0500

    html file changes to header file and style file

--- footer.php
+++ footer.php
@@ -43,32 +43,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php // Closing div#page from header.php. ?>
 </div><!-- #page -->
-<script>
-	jQuery(document).ready(function($) {
-    let currentSlide = 0;
-    const slides = $('.newest-blog-post .latest-post-wrapper');
-    const totalSlides = slides.length;
 
-    // Initially hide all slides except the first one
-    slides.hide();
-    slides.eq(currentSlide).show();
 
-    $('#next-post').click(function() {
-        slides.eq(currentSlide).fadeOut(400, function() {
-            currentSlide = (currentSlide + 1) % totalSlides; // Move to next slide
-            slides.eq(currentSlide).fadeIn(400); // Show the next slide
-        });
-    });
 
-    $('#prev-post').click(function() {
-        slides.eq(currentSlide).fadeOut(400, function() {
-            currentSlide = (currentSlide - 1 + totalSlides) % totalSlides; // Move to previous slide
-            slides.eq(currentSlide).fadeIn(400); // Show the previous slide
-        });
-    });
-});
-
-</script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -29,7 +29,8 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 	<!-- ******************* The Navbar Area ******************* -->
 	<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">New Blog Works</a>
+    <img src="http://science-blog.local/wp-content/uploads/2024/08/a8TRKU-science-beaker-clipart-getdrawings-free-download.png" alt="logo" width="50px" height="50px">
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">New Blog Works</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -47,15 +48,63 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 <div class="center-text">
 	<h2>Sign Up for Our Ideas Newsletter POV</h2>
 </div>
-<div class="gradient-overlay">
+<div class="container">
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div class="gradient-overlay">
     <div class="newest-blog-post">
         <?php echo do_shortcode('[latest_post posts_per_page="5"]'); ?>
     </div>
+
 </div>
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+    <div class="carousel-item">
+    <div class="gradient-overlay">
+    <div class="newest-blog-post">
+        <?php echo do_shortcode('[latest_post posts_per_page="5"]'); ?>
+    </div>
+
+</div>
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+    <div class="carousel-item">
+    <div class="gradient-overlay">
+    <div class="newest-blog-post">
+        <?php echo do_shortcode('[latest_post posts_per_page="5"]'); ?>
+    </div>
+
+</div>
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
 <div class="top-left">
-    <button id="prev-post" class="slider-btn">PREV</button>
-    <button id="next-post" class="slider-btn">NEXT</button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">PREV</button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">NEXT</button>
 </div>
-
-
+</div>
 
